0d9afd0e36752b23f73a98f3885f01afdee1e023
Avaq <aldwin.vlasblom@gmail.com>
2017-01-03 17:39:19 +0100

    Fix lint command and linting errors

--- inspect-f.js
+++ inspect-f.js
@@ -1,34 +1,34 @@
-(function(global, f){
+(function(global, f) {
 
   'use strict';
 
   /*istanbul ignore next*/
-  if(typeof module !== 'undefined'){
+  if(typeof module !== 'undefined') {
     module.exports = f();
-  }
-
-  else if(typeof global.define === 'function' && global.define.amd){
+  } else if(typeof global.define === 'function' && global.define.amd) {
     global.define([], f);
-  }
-
-  else{
+  } else{
     global.inspectf = f();
   }
 
-}(/*istanbul ignore next*/(global || window || this), function(){
+}(/*istanbul ignore next*/(global || window || this), () => {
 
   'use strict';
 
-  function checkn(n){
-    if(typeof n !== 'number') throw new TypeError(
+  function checkn(n) {
+    if(typeof n !== 'number') {
+      throw new TypeError(
       'inspectf expects its first argument to be a number'
     );
+    }
   }
 
-  function checkf(f){
-    if(typeof f !== 'function') throw new TypeError(
+  function checkf(f) {
+    if(typeof f !== 'function') {
+      throw new TypeError(
       'inspectf expects its second argument to be a function'
     );
+    }
   }
 
   const RSPACE = /^ */;
@@ -36,11 +36,11 @@
   const RTABS = /\t/g;
   const REOL = /\n\r?/;
 
-  function getPadding(line){
+  function getPadding(line) {
     return line.match(RSPACE)[0].length;
   }
 
-  function guessIndentation(lines){
+  function guessIndentation(lines) {
     const filtered = lines.filter(x => RCODE.test(x));
     const paddings = filtered.map(getPadding);
     const depth = paddings.reduce((a, b) => Math.min(a, b), Infinity);
@@ -48,19 +48,19 @@
     return {depth, tabsize};
   }
 
-  function pad(n){
+  function pad(n) {
     return (new Array(n + 1)).join(' ');
   }
 
-  function show(f, indentation){
+  function show(f, indentation) {
     return f.toString().replace(RTABS, indentation);
   }
 
-  function toLines(s){
+  function toLines(s) {
     return s.split(REOL);
   }
 
-  function fixIndentation(lines, indentation){
+  function fixIndentation(lines, indentation) {
     const info = guessIndentation(lines.slice(1));
     const RPAD = new RegExp(pad(info.tabsize), 'g');
     return lines.map(line =>
@@ -69,14 +69,14 @@
     ).join('\n');
   }
 
-  return function inspectf(n, f){
+  return function inspectf(n, f) {
     checkn(n);
-    if(arguments.length < 2) return f => inspectf(n, f);
+    if(arguments.length < 2) {return f => inspectf(n, f);}
     checkf(f);
-    if(f.toString !== Function.prototype.toString) return f.toString();
+    if(f.toString !== Function.prototype.toString) {return f.toString();}
     const i = pad(n), shown = show(f, i), lines = toLines(shown, i);
-    if(lines.length < 2) return shown;
+    if(lines.length < 2) {return shown;}
     return fixIndentation(lines, i);
-  }
+  };
 
 }));
